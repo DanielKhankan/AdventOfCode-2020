e3e0bb117f02c6dc100358dcd8a0f3f53972ef6b
Daniel Khankan <d.khankan@tricentis.com>
2020-12-11 00:43:06 +0100

    Day 10

---
+++ Day10.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode {
+    internal static class Day10 {
+        // 2176
+        internal static int Day10A()
+        {
+            var allNumbers = File.ReadAllLines(Path.Combine(Program.InputsFolder, "Day10.txt")).Select(int.Parse).ToList();
+
+            if (!allNumbers.Contains(0))
+            {
+                allNumbers.Add(0);
+            }
+
+            allNumbers.Add(allNumbers.Max() +3);
+            allNumbers.Sort();
+
+            var counter = (new List<int>(), new List<int>());
+            foreach (var number in allNumbers)
+            {
+                if (allNumbers.Contains(number+1))
+                {
+                    counter.Item1.Add(number+1);
+                }else if (allNumbers.Contains(number + 3))
+                {
+                    counter.Item2.Add(number + 3);
+                }
+            }
+
+            return counter.Item1.Count * counter.Item2.Count;
+        }
+
+        // 18512297918464
+        internal static long Day10B()
+        {
+            var allNumbers = File.ReadAllLines(Path.Combine(Program.InputsFolder, "Day10.txt")).Select(int.Parse).ToList();
+            if (!allNumbers.Contains(0))
+            {
+                allNumbers.Add(0);
+            }
+
+            allNumbers.Add(allNumbers.Max() + 3);
+            allNumbers.Sort();
+
+            var dictionary = new Dictionary<long, long>();
+
+            var counter = GetCount(allNumbers[^1]);
+
+            long GetCount(int number)
+            {
+                if (number == 0)
+                {
+                    return 1;
+                }
+
+                if (!allNumbers.Contains(number))
+                {
+                    return 0;
+                }
+
+                if (dictionary.ContainsKey(number))
+                {
+                    return dictionary[number];
+                }
+
+                var dic3 = GetCount(number - 3);
+                var dic2 = GetCount(number - 2);
+                var dic1 = GetCount(number - 1);
+
+                dictionary[number] = dic1 + dic2 + dic3;
+                return dictionary[number];
+            }
+            
+            return counter;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,15 +8,16 @@ namespace AdventOfCode {
 
         static void Main()
         {
-           Console.WriteLine($"Day1: A[{Day1.Day1A() == 224436}]\tB[{Day1.Day1B() == 303394260}]");
-           Console.WriteLine($"Day2: A[{Day2.Day2A() == 666}]\tB[{Day2.Day2B() == 670}]");
-           Console.WriteLine($"Day3: A[{Day3.Day3A() == 171}]\tB[{Day3.Day3B() == 1206576000}]");
-           Console.WriteLine($"Day4: A[{Day4.Day4A() == 170}]\tB[{Day4.Day4B() == 103}]");
-           Console.WriteLine($"Day5: A[{Day5.Day5A() == 994}]\tB[{Day5.Day5B() == 741}]");
-           Console.WriteLine($"Day6: A[{Day6.Day6A() == Day6.Day6A2() && Day6.Day6A2() == 6585}]\tB[{Day6.Day6B() == Day6.Day6B2() && Day6.Day6B2() == 3276}]");
-           Console.WriteLine($"Day7: A[{Day7.Day7A() == 192}]\tB[{Day7.Day7B() == 12128}]");
-           Console.WriteLine($"Day8: A[{Day8.Day8A() == 1654}]\tB[{Day8.Day8B() == 833}]");
-           Console.WriteLine($"Day9: A[{Day9.Day9A() == 70639851}]\tB[{Day9.Day9B() == 8249240}]");
+           Console.WriteLine($"Day1:\tA[{Day1.Day1A() == 224436}]\tB[{Day1.Day1B() == 303394260}]");
+           Console.WriteLine($"Day2:\tA[{Day2.Day2A() == 666}]\tB[{Day2.Day2B() == 670}]");
+           Console.WriteLine($"Day3:\tA[{Day3.Day3A() == 171}]\tB[{Day3.Day3B() == 1206576000}]");
+           Console.WriteLine($"Day4:\tA[{Day4.Day4A() == 170}]\tB[{Day4.Day4B() == 103}]");
+           Console.WriteLine($"Day5:\tA[{Day5.Day5A() == 994}]\tB[{Day5.Day5B() == 741}]");
+           Console.WriteLine($"Day6:\tA[{Day6.Day6A() == Day6.Day6A2() && Day6.Day6A2() == 6585}]\tB[{Day6.Day6B() == Day6.Day6B2() && Day6.Day6B2() == 3276}]");
+           Console.WriteLine($"Day7:\tA[{Day7.Day7A() == 192}]\tB[{Day7.Day7B() == 12128}]");
+           Console.WriteLine($"Day8:\tA[{Day8.Day8A() == 1654}]\tB[{Day8.Day8B() == 833}]");
+           Console.WriteLine($"Day9:\tA[{Day9.Day9A() == 70639851}]\tB[{Day9.Day9B() == 8249240}]");
+           Console.WriteLine($"Day10:\tA[{Day10.Day10A() == 2176}]\tB[{Day10.Day10B() == 18512297918464}]");
         }
     }
 }
